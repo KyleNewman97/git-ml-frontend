6f4ceaea606010aebb804db73cf8017eaa2bd8aa
KyleNewman97 <kyle@thenewmans.com.au>
2025-03-30 13:00:08 +0800

    feat(table): wip formatting

---
+++ src/components/Table.tsx
@@ -0,0 +1,33 @@
+export const Table = <T,>({
+  columnNames,
+  className,
+}: {
+  columnNames: string[];
+  className?: string;
+}) => {
+  return (
+    <table className={"border-separate border-spacing-0 " + className}>
+      <thead className="rounded-t-lg">
+        <tr>
+          {columnNames.map((columnName, index) => {
+            const roundingStyle =
+              index === 0
+                ? "rounded-tl-lg"
+                : index === columnNames.length - 1
+                  ? "rounded-tr-lg"
+                  : "";
+
+            return (
+              <th
+                className={`text-left py-4 pl-2 bg-table-main border border-table-border ${roundingStyle}`}
+                key={columnName}
+              >
+                {columnName}
+              </th>
+            );
+          })}
+        </tr>
+      </thead>
+    </table>
+  );
+};
--- src/pages/commits/CommitsPage.tsx
+++ src/pages/commits/CommitsPage.tsx
@@ -1,6 +1,19 @@
 import { useParams } from "react-router";
+import { Table } from "../../components/Table";
 
 export const CommitsPage = () => {
-    const params = useParams();
-    return <main>{`Commits page for ${params.repoName}`}</main>;
+  const params = useParams();
+
+  return (
+    <main className="bg-main text-primary-text w-full min-h-screen flex flex-col items-center">
+      <div className="max-w-2/3 w-full m-20 flex flex-col">
+        <p className="text-3xl font-bold">{params.repoName}</p>
+
+        <Table
+          columnNames={["Added", "Message", "Committer", "Hash"]}
+          className="w-full mt-10"
+        />
+      </div>
+    </main>
+  );
 };
